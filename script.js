80a45bf53ec102cb093d5e5e734e764f012de0bc
thisisazadi <a.msaboina@gmail.com>
2023-07-21 18:24:51 +0200

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+
+open.addEvenListener('click', () => container.classList.add('show-nav'));
+
+close.addEvenListener('click', () => container.classList.remove('show-nav'));
